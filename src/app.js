8f7882797a9539dc75906364332423db20b0bba1
MukundAgarwal-code <agarwalmukund1176@gmail.com>
2025-09-27 17:49:58 +0530

    huighu

--- src/app.js
+++ src/app.js
@@ -11,6 +11,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded( { extended: true } ));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.post("/api/analyse", upload.single("image"), mainController);
 
